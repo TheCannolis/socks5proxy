fb25020ced8b659ba35fa7f6e9091c8970932072
TheCannolis <theconfather@proton.me>
2026-07-24 08:10:44 +0200

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-"""WSGI entry points for production: gunicorn -b 127.0.0.1:8888 wsgi:admin_app"""
-import os, sys
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-from web_dashboard import app as admin_app
-from shop import app as shop_app
